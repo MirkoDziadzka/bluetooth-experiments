1ec041b5d10fafa53b9cb6199f9d4c76ebd41f7c
Mirko Dziadzka <mirko.dziadzka@gmail.com>
2020-06-21 22:56:55 +0200

    Initial Commit

---
+++ bluetoothTests/bluetoothTests.swift
@@ -0,0 +1,34 @@
+//
+//  bluetoothTests.swift
+//  bluetoothTests
+//
+//  Created by Mirko Dziadzka on 21.06.20.
+//  Copyright © 2020 Mirko Dziadzka. All rights reserved.
+//
+
+import XCTest
+@testable import bluetooth
+
+class bluetoothTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
